7bbe013822e49e60893162c786b98c74bb478b0d
Martin Ivanco <xivanc03@stud.fit.vutbr.cz>
2018-03-22 13:23:03 +0100

    Initial Commit

---
+++ Badmephisto's SpeedcubingTests/Badmephisto_s_SpeedcubingTests.swift
@@ -0,0 +1,36 @@
+//
+//  Badmephisto_s_SpeedcubingTests.swift
+//  Badmephisto's SpeedcubingTests
+//
+//  Created by Martin Ivančo on 22.3.18.
+//  Copyright © 2018 martinivanco. All rights reserved.
+//
+
+import XCTest
+@testable import Badmephisto_s_Speedcubing
+
+class Badmephisto_s_SpeedcubingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
